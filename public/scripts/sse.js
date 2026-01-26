7e6798d510fc0aba5b42680f3cb7e88743f9c35b
Gleb Matveev <2004galeb909@gmail.com>
2026-01-26 11:43:13 +0300

    fix: cache fix for static files (it was done before starting lab6 part 2 so it might change in next commit) and delete spec.ts

--- public/scripts/sse.js
+++ public/scripts/sse.js
@@ -1,15 +1,20 @@
-try {
-  if (window.EventSource) {
-    const source = new EventSource('/goal/events');
-    source.onmessage = (e) => {
-      try {
-        const evt = JSON.parse(e.data);
-        if (evt) {
-            console.log(evt);
-            const msg = `Goal with id(${evt.payload.id}) ${evt.type}`;
-            toastr.success(msg)
-        }
-      } catch (err) {}
-    };
-  }
-} catch (_) {}
+(() => {
+  if (!window.EventSource) return;
+
+  const source = new EventSource('/goal/events');
+
+  source.onmessage = (e) => {
+    try {
+      const evt = JSON.parse(e.data);
+      if (evt) {
+        const msg = `Goal with id(${evt.payload.id}) ${evt.type}`;
+        toastr.success(msg);
+      }
+    } catch (_) {}
+  };
+
+  // Close before unload to free the connection
+  window.addEventListener('beforeunload', () => {
+    source.close();
+  });
+})();
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe, ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  ValidationPipe,
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { join } from 'node:path';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/http-exception.filter';
-import "reflect-metadata";
+import 'reflect-metadata';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { RequestTimeInterceptor } from './common/request-time.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Swagger setup
   const config = new DocumentBuilder()
-   .setTitle('Sleep tracker API')
-   .setDescription('The sleep tracker API description')
-   .setVersion('1.0')
-   .addTag('sleep_tracker')
-   .build();
+    .setTitle('Sleep tracker API')
+    .setDescription('The sleep tracker API description')
+    .setVersion('1.0')
+    .addTag('sleep_tracker')
+    .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger', app, documentFactory);
 
@@ -38,14 +43,20 @@ async function bootstrap() {
   app.useGlobalInterceptors(new RequestTimeInterceptor());
 
   // Set up Handlebars as the view engine
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.js') || path.endsWith('.css')) {
+        res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+      }
+    },
+  });
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
   const hbs = require('hbs');
   hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
   hbs.registerHelper('json', (context) => JSON.stringify(context));
-  hbs.registerHelper('eq', function(a, b, options) {
+  hbs.registerHelper('eq', function (a, b, options) {
     if (a === b) {
       return options.fn(this);
     }
@@ -54,6 +65,6 @@ async function bootstrap() {
 
   // Start the server and log port
   await app.listen(process.env.PORT ?? 3000);
-  console.log("Server is listnening on port: ", process.env.PORT)
+  console.log('Server is listnening on port: ', process.env.PORT);
 }
 bootstrap();
